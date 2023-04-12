b8dadc1d776774955faec33b13377133bde90a1a
k3nss <123756039+k3nss@users.noreply.github.com>
2023-04-12 20:12:32 +0300

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,6 @@
+git add  // . - добавить все (Index.js определенный файл
+git commit -m // комментарий
+git push origin main // запушить ветку Main
+git branch // список веток
+git chekout main // переключиться на ветку Main 
+git chekout b new_branch // cоздать переключиться на ветку new_branch
