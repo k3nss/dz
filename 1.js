407519bf8472c453e3809dd1ac14b2f2eed5646a
k3nss <123756039+k3nss@users.noreply.github.com>
2023-04-12 20:13:19 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -3,4 +3,4 @@ git commit -m // комментарий
 git push origin main // запушить ветку Main
 git branch // список веток
 git chekout main // переключиться на ветку Main 
-git chekout b new_branch // cоздать переключиться на ветку new_branch
+git chekout b new_branch // cоздать и переключиться на ветку new_branch
