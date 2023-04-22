b0d59a96004f868c0d648803bcb884e6f570d722
k3nss <123756039+k3nss@users.noreply.github.com>
2023-04-22 14:54:47 +0300

    Update 3.js

--- 3.js
+++ 3.js
@@ -54,3 +54,19 @@ const a = bogatiri.map((a) => ({
 }))
 
 console.log(a)
+
+------------------------------------
+'use strict'
+
+const bogatiri = [
+    {id: 0, firstName: 'Ilya', lastName:'Mutomets', age: 58},
+    {id: 1, firstName: 'Alyosha', lastName: 'Popovich', age: 55},
+    {id: 2, firstName: 'Dobrinya', lastName: 'Nicitich', age: 53}
+]
+
+
+const sum1 =bogatiri.reduce((sum, a) => {
+  return sum + a.age
+}, 0)
+
+console.log(sum1)
