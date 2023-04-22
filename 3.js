54bd4ec4d0f6a18b5b5c997df0ae7d43481b2ddb
k3nss <123756039+k3nss@users.noreply.github.com>
2023-04-22 14:29:26 +0300

    Update 3.js

--- 3.js
+++ 3.js
@@ -17,3 +17,18 @@ const bogatiri = [
 
 const result = bogatiri.filter((item) => item.age >= 30);
 console.log(result)
+
+--------------------------------------
+'use strict'
+
+const bogatiri = [
+    {id: 0, firstName: 'Ilya', lastName:'Mutomets', age: 58},
+    {id: 1, firstName: 'Alyosha', lastName: 'Popovich', age: 55},
+    {id: 2, firstName: 'Dobrinya', lastName: 'Nicitich', age: 23}
+]
+
+        
+
+const j = bogatiri.every ((a) => (a.age >= 30))
+
+console.log(j)
