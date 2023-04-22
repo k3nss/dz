87a72ab3c4df092f594af4e5f74e745baef5a029
k3nss <123756039+k3nss@users.noreply.github.com>
2023-04-22 14:46:22 +0300

    Update 3.js

--- 3.js
+++ 3.js
@@ -38,3 +38,19 @@ const bogatiri = [
 const j = bogatiri.every ((a) => (a.age >= 30))
 
 console.log(j)
+
+----------------------------------
+
+'use strict'
+
+const bogatiri = [
+    {id: 0, firstName: 'Ilya', lastName:'Mutomets', age: 58},
+    {id: 1, firstName: 'Alyosha', lastName: 'Popovich', age: 55},
+    {id: 2, firstName: 'Dobrinya', lastName: 'Nicitich', age: 53}
+]
+
+const a = bogatiri.map((a) => ({
+    ...a, firstName: a.firstName.toUpperCase()
+}))
+
+console.log(a)
