e7c9a919302e4818c1727ba0b73a6aec9d4ec0bf
k3nss <123756039+k3nss@users.noreply.github.com>
2023-04-24 16:01:07 +0300

    Update 5.js

--- 5.js
+++ 5.js
@@ -1,1 +1,11 @@
+const obj = {
+  name: "Ferrari",
+  model: "LaFerrari",
+  color: "Red"
+}
 
+function getFullName(mileage){
+  return this.name + " " + this.model + " " + this.color + " " + mileage
+}
+
+getFullName.apply(obj, [14455])
