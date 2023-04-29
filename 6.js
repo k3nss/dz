65fc774e5bc2267c0a1e065bcc25f23eb8625a17
k3nss <123756039+k3nss@users.noreply.github.com>
2023-04-29 14:49:15 +0300

    Update 6.js

--- 6.js
+++ 6.js
@@ -1,17 +1,22 @@
 class User{
-  name = 'Ivan'
-  surname = 'Ivanov'  
+  constructor(name, surname){
+    this.name = name
+    this.surname = surname
+  }
     getFullName(){
     return this.name + ' ' + this.surname
   }
 }
 
 class UserAdress extends User{
-  city = 'Kazan'
+  constructor(adress, name, surname){
+    super(name,surname)
+    this.adress = adress
+  }
     getFullName(){
-    return 'Ваше имя - ' + this.name + ' ' + this.surname + '. Ваш адрес - город ' + this.city
+    return 'Ваше имя - ' + this.name + ' ' + this.surname + '. Ваш адрес - город ' + this.adress
   }
 }
 
-const user = new UserAdress()
+const user = new UserAdress('Казань', 'Иван', 'Иванов')
 alert(user.getFullName())
