f5cce5eb9e3b8d073a7fdf89fed76ff075ee74f4
k3nss <123756039+k3nss@users.noreply.github.com>
2023-05-02 19:48:18 +0300

    Delete github.js

--- github.js
+++
@@ -1,7 +0,0 @@
-git add  // . - добавить все (Index.js определенный файл
-git commit -m // комментарий
-git push origin main // запушить ветку Main
-git branch // список веток
-git chekout main // переключиться на ветку Main 
-git chekout b new_branch // cоздать и переключиться на ветку new_branch
-git clone // склонировать репозиторий
